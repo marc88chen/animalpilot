8569b0a0e85cac9e1735e53e93d4479a89242746
marc88chen <66193562+marc88chen@users.noreply.github.com>
2023-11-02 13:11:31 +0800

    Update interfaces.py

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -15,7 +15,7 @@ GearShifter = car.CarState.GearShifter
 EventName = car.CarEvent.EventName
 
 MAX_CTRL_SPEED = (V_CRUISE_MAX + 4) * CV.KPH_TO_MS
-ACCEL_MAX = 2.0
+ACCEL_MAX = 3.5
 ACCEL_MIN = -3.5
 
 
